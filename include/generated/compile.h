4f8469125ec4c2479b757abf84428bbc39bf2a57
jeikerxiao <jeiker@126.com>
2026-07-17 12:43:05 +0800

    init linux study code
    init linux study code

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 9 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#9 SMP PREEMPT Wed Jun 19 15:29:15 CST 2024"
+#define LINUX_COMPILE_BY "alientek"
+#define LINUX_COMPILE_HOST "Ubuntu16"
+#define LINUX_COMPILER "gcc version 4.9.4 (Linaro GCC 4.9-2017.01) "
